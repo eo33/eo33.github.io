ea9bd7d0eddda7e8f1577fc68a3fba070e9ec5c2
lasto-ed12 <70526829+eo33@users.noreply.github.com>
2024-02-19 00:50:07 +1100

    added latest changes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'animate.css/animate.min.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.min.css";
 import "./HomePage.css";
 
 // Components
@@ -17,93 +17,108 @@ import twSeoSkills from "../../textContent/twSeoSkills.json";
 import experience from "../../textContent/experience.json";
 import education from "../../textContent/education.json";
 
+function HomePage() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const skillsSection = document.getElementById("firstpage-skills");
+      const experienceSection = document.getElementById("firstpage-experience");
+      const sections = [skillsSection, experienceSection];
 
-function HomePage(){
+      sections.forEach((section) => {
+        const position = section.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3;
 
-    useEffect(() => {
-        const handleScroll = () => {
-          const skillsSection = document.getElementById("firstpage-skills");
-          const experienceSection = document.getElementById("firstpage-experience");
-          const sections = [skillsSection, experienceSection];
-      
-          sections.forEach((section) => {
-            const position = section.getBoundingClientRect().top;
-            const screenPosition = window.innerHeight / 1.3;
-      
-            if (position < screenPosition) {
-              section.classList.add("animate__fadeInLeft");
-              section.classList.remove("hidden");
-            } else {
-              section.classList.remove("animate__fadeInLeft");
-              section.classList.add("hidden");
-            }
-          });
-        };
-      
-        window.addEventListener("scroll", handleScroll);
-      
-        return () => {
-          window.removeEventListener("scroll", handleScroll);
-        };
-      }, []);
-      
-      
-    return(
-        <div className="add-scrollbar">
-            <Banner content={desc}/>
-            <div id="background-color">
-                <div className="container">
-                    {/*About Me page*/}
-                    <div className="row justify-content-center d-flex">
-                        <div className="col-lg-6 col-sm-9 p-4 ">
-                            <img 
-                                src="./assets/img.jpg" 
-                                alt="Image" 
-                                className="img-fluid rounded border border-dark animate__animated animate__fadeInLeft fade-in-left"
-                            />
-                        </div>
-                        <div className="col-lg-6 col-sm-9 d-flex-col justify-content-center align-items-center p-3">
-                            <h3 className=" text-white display-4 about-me">{strings.aboutTitle}</h3>
-                            <h5 className="lead text-white mt-3 py-2 ">{writtenContent.introduction}</h5>
-                            <div className="d-flex justify-content-end mt-2 mt-5">
-                                <a href="./assets/RESUME_WD.pdf" download="EO_resume.pdf">
-                                    <button type="button" class="btn btn-primary mt-4 button-bg mb-3">{strings.downloadResume}</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+        if (position < screenPosition) {
+          section.classList.add("animate__fadeInLeft");
+          section.classList.remove("hidden");
+        } else {
+          section.classList.remove("animate__fadeInLeft");
+          section.classList.add("hidden");
+        }
+      });
+    };
 
-                    <div className="row text-white mt-md-5 py-md-5" id="firspage-webdev-skills">
-                            <h4 className="display-5 mt-5 ">{strings.skillsTitle}</h4>
-                    </div>
-                    
-                    <div className="animate__animated animate__fadeInLeft fade-in-left animate-left-scroll" id="firstpage-skills">
-                        {/*Skill section*/}
-                        <div className="row text-white mt-1">
-                            <p className="text-center display-6">{strings.skillsHeading_1}</p>
-                        </div>
-                        {/*Web dev cards*/}
-                        <Cards imagesPath={webDevSkills}/>
-                        {/*Tech writing skills*/}
-                        <div className="row text-white mt-2">
-                            <p className="text-center display-6">{strings.skillsHeading_2}</p>
-                        </div>
-                        {/*TW SEO cards*/}
-                        <Cards imagesPath={twSeoSkills}/>
-                    </div>
+    window.addEventListener("scroll", handleScroll);
 
-                    {/*Education and Experience*/}
-                    <div id="firstpage-experience-education"></div>
-                    <div id="firstpage-experience" className="animate__animated animate__fadeInLeft fade-in-left animate-left-scroll">
-                        <div className="row text-white mt-5 px-2">  
-                            <CareerDesc desc={experience} title="Experience"/>
-                            <CareerDesc desc={education} title="Education"/>
-                        </div>
-                    </div>
-                </div>
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="add-scrollbar">
+      <Banner content={desc} />
+      <div id="background-color">
+        <div className="container">
+          {/*About Me page*/}
+          <div className="row justify-content-center d-flex">
+            <div className="col-lg-6 col-sm-9 p-4 ">
+              <img
+                src="./assets/img.jpg"
+                alt="Image"
+                className="img-fluid rounded border border-dark animate__animated animate__fadeInLeft fade-in-left"
+              />
             </div>
+            <div className="col-lg-6 col-sm-9 d-flex-col justify-content-center align-items-center p-3">
+              <h3 className=" text-white display-4 about-me">
+                {strings.aboutTitle}
+              </h3>
+              <h5 className="lead text-white mt-3 py-2 ">
+                {writtenContent.introduction}
+              </h5>
+              <div className="d-flex justify-content-end mt-2 mt-5">
+                <a href="./assets/RESUME_EdO.pdf" download="Resume.pdf">
+                  <button
+                    type="button"
+                    class="btn btn-primary mt-4 button-bg mb-3"
+                  >
+                    {strings.downloadResume}
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="row text-white mt-md-5 py-md-5"
+            id="firspage-webdev-skills"
+          >
+            <h4 className="display-5 mt-5 ">{strings.skillsTitle}</h4>
+          </div>
+
+          <div
+            className="animate__animated animate__fadeInLeft fade-in-left animate-left-scroll"
+            id="firstpage-skills"
+          >
+            {/*Skill section*/}
+            <div className="row text-white mt-1">
+              <p className="text-center display-6">{strings.skillsHeading_1}</p>
+            </div>
+            {/*Web dev cards*/}
+            <Cards imagesPath={webDevSkills} />
+            {/*Tech writing skills*/}
+            <div className="row text-white mt-2">
+              <p className="text-center display-6">{strings.skillsHeading_2}</p>
+            </div>
+            {/*TW SEO cards*/}
+            <Cards imagesPath={twSeoSkills} />
+          </div>
+
+          {/*Education and Experience*/}
+          <div id="firstpage-experience-education"></div>
+          <div
+            id="firstpage-experience"
+            className="animate__animated animate__fadeInLeft fade-in-left animate-left-scroll"
+          >
+            <div className="row text-white mt-5 px-2">
+              <CareerDesc desc={experience} title="Experience" />
+              <CareerDesc desc={education} title="Education" />
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,9 +1,9 @@
-import React,{useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.css'
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./ProjectsPage.css";
-import {Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-import projectDescription from "../../textContent/projectDescription.json"
+import projectDescription from "../../textContent/projectDescription.json";
 
 const ProjectsPage = () => {
   return (
@@ -11,33 +11,35 @@ const ProjectsPage = () => {
       <div className="container text-white">
         {/*Title*/}
         <div className="row mt-4 mb-2">
-          <div className="col display-5">
-            Projects
-          </div>
+          <div className="col display-5">Projects</div>
         </div>
         {/*Project cards*/}
         <div className="row row-cols-1 row-cols-md-2 justify-content-center">
-          {
-            projectDescription.map(key=>( 
-              <div className="col g-4 mb-3">
-                <div className="card h-100 bg-transparent text-white border-0">
-                  <div className="card-body">
-                    <h5 className="card-title text-center fs-4 mb-4">{key.title}</h5>
-                    <Link to={`${key.title.toLowerCase().replace(/ /g, "-")}`}>
-                      <img src={key.image} alt={`${key.title} image`} className="card-img-top rounded" />
-                    </Link>
-                    <p class="card-text mt-3 p-2 fs-5">
-                      {key.description}
-                    </p>
-                  </div>
+          {projectDescription.map((key) => (
+            <div className="col g-4 mb-3">
+              <div className="card h-100 bg-transparent text-white border-0">
+                <div className="card-body">
+                  <h5 className="card-title text-center fs-4 mb-4">
+                    {key.title}
+                  </h5>
+                  <Link to={`${key.title.toLowerCase().replace(/ /g, "-")}`}>
+                    <img
+                      src={key.image}
+                      alt={`${key.title} image`}
+                      className="card-img-top rounded"
+                    />
+                  </Link>
+                  <p class="card-text mt-3 p-2 fs-5 text-center">
+                    {key.description}
+                  </p>
                 </div>
               </div>
-            ))
-          }
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default ProjectsPage;
